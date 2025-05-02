799969127b0bddf0ef6b344e8238a34f4d86cceb
Eduard Dumitru <dumitrueduard40@gmail.com>
2025-05-02 12:35:39 +0300

    Setting up DiamondKata solution + first unit test

---
+++ DiamondKata/DiamondKata/DiamondKata.Tests/DiamondKataTest.cs
@@ -0,0 +1,13 @@
+﻿namespace DiamondKata.Tests
+{
+    public class DiamondKataTest
+    {
+        [Test]
+        public void TestInputAcceptsOnlyLetter()
+        {
+            var diamond = new DiamondKata();
+            var result = diamond.CreateDiamond('1');
+            Assert.Throws<ArgumentException>(() => diamond.CreateDiamond('1'));
+        }
+    }
+}
---
+++ DiamondKata/DiamondKata/DiamondKata/DiamondKata.cs
@@ -0,0 +1,10 @@
+﻿namespace DiamondKata
+{
+    public class DiamondKata
+    {
+        public string CreateDiamond(char letter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
